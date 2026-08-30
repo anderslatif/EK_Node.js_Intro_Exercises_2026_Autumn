4696ad30821a69a0d625d8a630c87dccef2a3148
SGyldenkvist <silke@gyldenkvist.dk>
2026-08-30 17:08:22 +0200

    Complete intro exercises

--- 01._Introduction_Exercises/4._Arrays.js
+++ 01._Introduction_Exercises/4._Arrays.js
@@ -6,6 +6,8 @@
 const letters = ["a", "b", "c"];
 // show b in the console 
 
+console.log(letters[1]);
+
 
 // --------------------------------------
 // Exercise 2 - Array Positioning
@@ -13,6 +15,8 @@ const letters = ["a", "b", "c"];
 const friends = [];
 
 // What a lonely array. Add at least 3 friend objects to it.  
+friends.push({name:"Olivia"},{name:"Janus"}, {name:"Olga"});
+console.log(friends);
 
 
 // --------------------------------------
@@ -22,7 +26,9 @@ const significantMathNumbers = [0, 2.718, 3.14159, 1729];
 
 // You want to programmatically find where the number 1729 is in the array.
 // programmatically means that no finger counting allowed. There is a method for this (finding index based of value). 
+const firstOccurence =significantMathNumbers.indexOf(1729);
 
+console.log(firstOccurence);
 
 // --------------------------------------
 // Exercise 4 - Inserting elements
@@ -30,6 +36,11 @@ const significantMathNumbers = [0, 2.718, 3.14159, 1729];
 const diet = ["tomato", "cucumber", "rocket"];
 
 // You are a programmer. In one line (one statement) insert hamburger, soda and pizza between the elements cucumber and rocket
+
+diet.splice(2,0, "hamburger", "soda", "pizza");
+
+console.log(diet);
+
 
 
 
@@ -39,6 +50,9 @@ const diet = ["tomato", "cucumber", "rocket"];
 
 // Remove the LAST element of the array.
 // Don't remove by index. You know in advance that it's the last in the array because you are too full already. 
+diet.pop();
+
+console.log(diet);
 
 
 
@@ -49,7 +63,8 @@ const diet = ["tomato", "cucumber", "rocket"];
 
 // You really like your daily diet from last exercise. Copy it to a new array called dinnerTray so you can give it to a friend.  
 
-
+const dinnerTray = diet.slice();
+console.log(dinnerTray);
 
 // --------------------------------------
 // Exercise 7 - For loop
@@ -57,6 +72,11 @@ const diet = ["tomato", "cucumber", "rocket"];
 const lettersExpanded = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
 // log every second char in the array starting from b
+
+for (let i =1; i < lettersExpanded.length; i+=2){
+
+    console.log(lettersExpanded[i]);
+}
 
 
 
@@ -69,6 +89,15 @@ const discardedNumbers = [];
 
 // log the element if the number is above 6 or below 0
 // else push them to the array discardedNumbers
+for(let i = 0; i < numbers.length; i++){
+    if (numbers[i] > 6 || numbers[i] < 0){
+        console.log(numbers[i]);
+    }else{
+        discardedNumbers.push(numbers[i]);
+    }
+
+}
+
 
 // --------------------------------------
 
