6720da960eff9f9b0d7883b18fdc6992962f627e
gidgud <jonathanbirkstrom@gmail.com>
2026-08-30 19:01:35 +0200

    Done with exercises

--- 01._Introduction_Exercises/1._Basics.js
+++ 01._Introduction_Exercises/1._Basics.js
@@ -10,7 +10,7 @@ const lastName = "Latif";
 // show in the console
 // My first name is Anders and my last name is Latif
 
-
+console.log(`My first name is ${firstName} and my last name is ${lastName}`);
 
 // --------------------------------------
 // Exercise 2 - Numbers and Strings
@@ -22,5 +22,6 @@ const increment = 1;
 // The result should be 2026
 // You cannot touch the first or the second line
 
+console.log(Number(year) + increment);
 
 // --------------------------------------
