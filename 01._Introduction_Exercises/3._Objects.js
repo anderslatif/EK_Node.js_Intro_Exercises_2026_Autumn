c6aab077fd8c12f1e1fdd7fe29295ed7fd7a0595
MaVirgil <magnusvirgil@hotmail.com>
2026-08-31 11:48:25 +0200

    solve exercises

--- 01._Introduction_Exercises/3._Objects.js
+++ 01._Introduction_Exercises/3._Objects.js
@@ -5,13 +5,19 @@
 
 const greetings = { message: "Hello, earthling! I bring peace." };
 
-// Log the message 
+// Log the message
+
+console.log(greetings.message) //OUTPUT => Hello, earthling! I bring peace.
 
 // --------------------------------------
 // Exercise 2 - Defining an object.
 
 // Create an object that has your name and age. 
 
+const myself = {
+  name: "Magnus Sørensen",
+  age: 28
+}
 
 // --------------------------------------
 // Exercise 3 - Add a key-value pair
@@ -20,6 +26,8 @@ const stackOverflow = {};
 
 // make a rule called isAllowed and let the value be true
 
+stackOverflow.isAllowed = true;
+
 // --------------------------------------
 // Exercise 4 - Remove a property
 
@@ -27,6 +35,11 @@ const thisSong = { description: "The best song in the world." };
 
 // remove the property "description" and add a property called "about" that should say "Just a tribute." 
 
+delete thisSong.description;
+
+thisSong.about = "Just a tribute"
+
+console.log(thisSong) //OUTPUT => { about: 'Just a tribute' }
 
 // --------------------------------------
 
