56d145fd7ec26010eb084ae6fc16236816b08d6b
PigeonPC <olgakatinka@gmail.com>
2026-08-31 18:39:44 +0200

    Opgaver besvaret

--- 01._Introduction_Exercises/3._Objects.js
+++ 01._Introduction_Exercises/3._Objects.js
@@ -7,18 +7,32 @@ const greetings = { message: "Hello, earthling! I bring peace." };
 
 // Log the message 
 
+console.log(greetings.message);
+
 // --------------------------------------
 // Exercise 2 - Defining an object.
 
 // Create an object that has your name and age. 
 
 
+const nameAndAge = {
+    name : "Olga",
+    age : 27
+};
+
+console.log(nameAndAge);
+
 // --------------------------------------
 // Exercise 3 - Add a key-value pair
 
-const stackOverflow = {};
+const stackOverflow = {
+
+};
 
 // make a rule called isAllowed and let the value be true
+
+stackOverflow.isAllowed = true;
+console.log(stackOverflow);
 
 // --------------------------------------
 // Exercise 4 - Remove a property
@@ -27,6 +41,11 @@ const thisSong = { description: "The best song in the world." };
 
 // remove the property "description" and add a property called "about" that should say "Just a tribute." 
 
+delete thisSong.description;
+thisSong.about = `This is just a tribute, you've gotta believe me. 
+    And I wish you were there, just a matter of opinion`;
+
+console.log(thisSong);
 
 // --------------------------------------
 
